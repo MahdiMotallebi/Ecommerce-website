73d33d828f9091143dd987fb73366153e9e4b1a3
mahdi <mahdimotallebe@gmail.com>
2022-06-14 18:26:12 +0430

    change style loading

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   }, []);
   return (
     <>
-      {state.loading === "loading" || state.loading === "" ? (
+      {state.loading === "loading" || state.loading === "faild" ? (
         <Loading />
       ) : (
         <>
